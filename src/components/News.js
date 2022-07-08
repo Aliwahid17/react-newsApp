4d7cd9a07133b93fa7ec1dd01d9884639a4ff01d
wahid ali <88263575+Aliwahid17@users.noreply.github.com>
2022-07-08 14:00:59 +0530

    first commit

---
+++ src/components/News.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+
+export class News extends Component {
+    render() {
+        return (
+            <div className='container my-3'>
+                <h2>NewsMonkey - Top headlines</h2>
+                <div className="row">
+                    <div className="col-md-4">
+                        <NewsItem title="mytitle" description="mydesc" />
+                    </div>
+                    <div className="col-md-4">
+                        <NewsItem title="mytitle" description="mydesc" />
+                    </div>
+                    <div className="col-md-4">
+                        <NewsItem title="mytitle" description="mydesc" />
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default News
