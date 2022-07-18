e20da254630a310891f9a88afe0a9facca388622
wahid ali <88263575+Aliwahid17@users.noreply.github.com>
2022-07-18 12:34:36 +0530

    added infinite scrolling and top loading bar

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,7 +6,16 @@ export class NewsItem extends Component {
         return (
             <div className="my-3">
                 <div className="card">
-                    <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{ left: '90%', zIndex: '1' }}>{source}</span>
+                    <div style={
+                        {
+                            display:'flex',
+                            justifyContent:'flex-end',
+                            position:'absolute',
+                            right:'0'
+                        }
+                    }>
+                    <span className="badge rounded-pill bg-danger">{source}</span>
+                    </div>
                     <img src={!imageUrl ? "https://i.stack.imgur.com/mwFzF.png" : imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}...</h5>
